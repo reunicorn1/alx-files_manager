848861bd5f030f4c0cf04d025ef411d3fa6ee784
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-08-05 13:00:19 +0400

    mongodb utils: fixing eslint and importing the dependencies

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,4 @@
-import MongoClient from 'mongodb/lib/mongo_client.js';
+import MongoClient from 'mongodb/lib/mongo_client';
 
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 27017;
