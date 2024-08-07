c59da924e945d1e3ebbc945dd15b2f9432874139
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-08-07 19:10:18 +0400

    troubleshoot #1: removing filecollection completetly

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getUserBase, getUserX, getFile } from '../middlewares/authMiddleware';
+import { getUserBase, getUserX } from '../middlewares/authMiddleware';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
@@ -12,43 +12,25 @@ router.route('/status')
   .get((req, res) => {
     AppController.getStatus(req, res);
   });
-
 // GET /stats => AppController.getStats
 router.route('/stats')
   .get((req, res) => {
     AppController.getStats(req, res);
   });
-
 // POST /users => UsersController.postNew
 router.route('/users')
   .post((req, res) => {
     UsersController.postNew(req, res);
   });
-
-// Middlware related routes ---------------------------------------
-
 // POST /files => FilesController.postUpload
-router.post('/files', getUserX, FilesController.postUpload);
-
+router.route('/files').post((req, res) => {
+  FilesController.postUpload(req, res);
+});
+// Middlware related routes
 // GET /connect => AuthController.getConnect
 router.get('/connect', getUserBase, AuthController.getConnect);
-
 // GET /disconnect => AuthController.getDisconnect
 router.get('/disconnect', getUserX, AuthController.getDisconnect);
-
 // GET /users/me => UserController.getMe~
 router.get('/users/me', getUserX, UsersController.getMe);
-
-// GET /files/:id => FilesController.getShow
-router.get('/files/:id', getUserX, getFile, FilesController.getShow);
-
-// GET /files => FilesController.getIndex
-router.get('/files', getUserX, FilesController.getIndex);
-
-// PUT /files/:id/publish => FilesController.putPublish
-router.put('/files/:id/publish', getUserX, getFile, FilesController.putPublish);
-
-// PUT /files/:id/publish => FilesController.putUnpublish
-router.put('/files/:id/unpublish', getUserX, getFile, FilesController.putUnpublish);
-
 export default router;
